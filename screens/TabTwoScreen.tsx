cb6f72716a715bcff145eecf015aba3f3dae9fff
lucas lima <lucaslavey@gmail.com>
2022-12-11 19:19:29 -0300

    refactor navigation

--- screens/TabTwoScreen.tsx
+++ screens/TabTwoScreen.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 import { StyleSheet, Text, View, TouchableOpacity, StatusBar } from 'react-native';
 
 import { Agenda } from 'react-native-calendars';
-import Navigation from '../navigation';
+import { todayString } from 'react-native-calendars/src/expandableCalendar/commons';
 
 
 const timeToString = (time: string | number | Date) => {
@@ -20,110 +20,46 @@ const App = () => {
   const navigation = useNavigation();
 
 
-    const [items, setItems] = React.useState({});
+    const [items, setItems] = React.useState<any>({});
 
 
-    const loadItems = (day: { timestamp: number; }) => {
+    const loadItems = () => {
+        
 
-
-        setTimeout(() => {
-
-            for (let i = -15; i < 85; i++) {
-
-                const time = day.timestamp + i * 24 * 60 * 60 * 1000;
-
-                const strTime = timeToString(time);
-
-
-                if (!items[strTime]) {
-
-                    items[strTime] = [];
-
-
-                    const numItems = Math.floor(Math.random() * 3 + 1);
-
-                    for (let j = 0; j < numItems; j++) {
-
-                        items[strTime].push({
-
-                            name: 'Item for ' + strTime + ' #' + j,
-
-                            height: Math.max(10, Math.floor(Math.random() * 150)),
-
-                            day: strTime
-
-                        });
-
-                    }
-
-                }
-
-            }
-
-            const newItems = {};
-
-            Object.keys(items).forEach(key => {
-
-                newItems[key] = items[key];
-
-            });
-
-            setItems(newItems);
-
-        }, 1000);
-
+              const item={
+                '2012-05-22': [{name: 'item 1 - any js object'}],
+                '2012-05-23': [{name: 'item 2 - any js object', height: 80}],
+                '2012-05-24': [],
+                '2012-05-25': [{name: 'item 3 - any js object'}, {name: 'any js object'}]
+              }
+          
+              setItems(item);
     }
 
+    const renderItem = (items: { name: string | number | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | React.ReactFragment | null | undefined; }) => {
 
-    const renderItem = (item: { name: any}) => {
         return (
-
             <TouchableOpacity style={styles.item}
-            onPress={() => navigation.navigate('Modal', {item: item.name})
-            
-          }
-            
-            >
-
-
+            onPress={() => navigation.navigate('Modal', {item: items.name})}>
                         <View>
-
-                            <Text>{item.name}</Text>
-
+                            <Text>item</Text>
                         </View>
-
-                
-
             </TouchableOpacity>
 
         );
 
     }
-
-
     return (
 
         <View style={styles.container}>
-
             <Agenda
-
                 items={items}
-
                 loadItemsForMonth={loadItems}
-
-                selected={'2022-07-07'}
-
-
                 showClosingKnob={true}
-
                 refreshing={false}
-
                 renderItem={renderItem}
-
             />
-
             <StatusBar />
-
         </View>
 
     );
@@ -134,23 +70,16 @@ const App = () => {
 const styles = StyleSheet.create({
 
     container: {
-
         flex: 1,
-
     },
 
     item: {
-
+      backgroundColor: '#fff',
         flex: 1,
-
         borderRadius: 5,
-
         padding: 10,
-
         marginRight: 10,
-
         marginTop: 17
-
     },
 
 });
