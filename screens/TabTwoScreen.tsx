94000efed356c317a61d540411249203ce51d66f
lucas lima <lucaslavey@gmail.com>
2022-12-11 12:41:52 -0300

    add navigation

--- screens/TabTwoScreen.tsx
+++ screens/TabTwoScreen.tsx
@@ -1,138 +1,159 @@
-import { useNavigation } from "@react-navigation/native";
-import React, { Component } from "react";
-import { Alert, StyleSheet, Text, View, TouchableOpacity } from "react-native";
-import {
-  Agenda,
-  DateData,
-  AgendaEntry,
-  AgendaSchedule,
-} from "react-native-calendars";
-import { RootStackParamList } from "../types";
+import { useNavigation } from '@react-navigation/native';
+import React from 'react';
 
-interface State {
-  items?: AgendaSchedule;
-}
+import { StyleSheet, Text, View, TouchableOpacity, StatusBar } from 'react-native';
 
-export default class AgendaScreen extends Component<State> {
-  state: State = {
-    items: undefined,
-  };
+import { Agenda } from 'react-native-calendars';
+import Navigation from '../navigation';
 
-  // reservationsKeyExtractor = (item, index) => {
-  //   return `${item?.reservation?.day}${index}`;
-  // };
 
-  render() {
-    return (
-      <Agenda
-        items={this.state.items}
-        loadItemsForMonth={this.loadItems}
-        renderItem={this.renderItem}
-        renderEmptyDate={this.renderEmptyDate}
-        rowHasChanged={this.rowHasChanged}
-        showClosingKnob={true}
-        // markingType={'period'}
-        // markedDates={{
-        //    '2017-05-08': {textColor: '#43515c'},
-        //    '2017-05-09': {textColor: '#43515c'},
-        //    '2017-05-14': {startingDay: true, endingDay: true, color: 'blue'},
-        //    '2017-05-21': {startingDay: true, color: 'blue'},
-        //    '2017-05-22': {endingDay: true, color: 'gray'},
-        //    '2017-05-24': {startingDay: true, color: 'gray'},
-        //    '2017-05-25': {color: 'gray'},
-        //    '2017-05-26': {endingDay: true, color: 'gray'}}}
-        // monthFormat={'yyyy'}
-        // theme={{calendarBackground: 'red', agendaKnobColor: 'green'}}
-        //renderDay={(day, item) => (<Text>{day ? day.day: 'item'}</Text>)}
-        // hideExtraDays={false}
-        // showOnlySelectedDayItems
-        // reservationsKeyExtractor={this.reservationsKeyExtractor}
-      />
-    );
-  }
+const timeToString = (time: string | number | Date) => {
 
-  loadItems = (day: DateData) => {
-    const items = this.state.items || {};
-
-    setTimeout(() => {
-      for (let i = -15; i < 85; i++) {
-        const time = day.timestamp + i * 24 * 60 * 60 * 1000;
-        const strTime = this.timeToString(time);
-
-        if (!items[strTime]) {
-          items[strTime] = [];
-
-          const numItems = Math.floor(Math.random() * 3 + 1);
-          for (let j = 0; j < numItems; j++) {
-            items[strTime].push({
-              name: "Item for " + strTime + " #" + j,
-              height: Math.max(50, Math.floor(Math.random() * 150)),
-              day: strTime,
-            });
-          }
-        }
-      }
-
-      const newItems: AgendaSchedule = {};
-      Object.keys(items).forEach((key) => {
-        newItems[key] = items[key];
-      });
-      this.setState({
-        items: newItems,
-      });
-    }, 1000);
-  };
-
-  renderItem = (reservation: AgendaEntry, isFirst: boolean) => {
-    const fontSize = isFirst ? 16 : 14;
-    const color = isFirst ? "black" : "#43515c";
-    const navigation = useNavigation<RootStackParamList>();
-
-    return (
-      <TouchableOpacity
-        style={[styles.item, { height: reservation.height }]}
-        onPress={() => 
-          navigation.navigate("Modal", {
-            reservation: reservation.name,
-            day: reservation.day,
-          })
-        }
-      >
-        <Text style={{ fontSize, color }}>{reservation.name}</Text>
-      </TouchableOpacity>
-    );
-  };
-
-  renderEmptyDate = () => {
-    return (
-      <View style={styles.emptyDate}>
-        <Text>This is empty date!</Text>
-      </View>
-    );
-  };
-
-  rowHasChanged = (r1: AgendaEntry, r2: AgendaEntry) => {
-    return r1.name !== r2.name;
-  };
-
-  timeToString(time: number) {
     const date = new Date(time);
-    return date.toISOString().split("T")[0];
-  }
+
+    return date.toISOString().split('T')[0];
+
 }
+
+
+const App = () => {
+  const navigation = useNavigation();
+
+
+    const [items, setItems] = React.useState({});
+
+
+    const loadItems = (day: { timestamp: number; }) => {
+
+
+        setTimeout(() => {
+
+            for (let i = -15; i < 85; i++) {
+
+                const time = day.timestamp + i * 24 * 60 * 60 * 1000;
+
+                const strTime = timeToString(time);
+
+
+                if (!items[strTime]) {
+
+                    items[strTime] = [];
+
+
+                    const numItems = Math.floor(Math.random() * 3 + 1);
+
+                    for (let j = 0; j < numItems; j++) {
+
+                        items[strTime].push({
+
+                            name: 'Item for ' + strTime + ' #' + j,
+
+                            height: Math.max(10, Math.floor(Math.random() * 150)),
+
+                            day: strTime
+
+                        });
+
+                    }
+
+                }
+
+            }
+
+            const newItems = {};
+
+            Object.keys(items).forEach(key => {
+
+                newItems[key] = items[key];
+
+            });
+
+            setItems(newItems);
+
+        }, 1000);
+
+    }
+
+
+    const renderItem = (item: { name: any}) => {
+        return (
+
+            <TouchableOpacity style={styles.item}
+            onPress={() => navigation.navigate('Modal', {item: item.name})
+            
+          }
+            
+            >
+
+
+                        <View>
+
+                            <Text>{item.name}</Text>
+
+                        </View>
+
+                
+
+            </TouchableOpacity>
+
+        );
+
+    }
+
+
+    return (
+
+        <View style={styles.container}>
+
+            <Agenda
+
+                items={items}
+
+                loadItemsForMonth={loadItems}
+
+                selected={'2022-07-07'}
+
+
+                showClosingKnob={true}
+
+                refreshing={false}
+
+                renderItem={renderItem}
+
+            />
+
+            <StatusBar />
+
+        </View>
+
+    );
+
+}
+
 
 const styles = StyleSheet.create({
-  item: {
-    backgroundColor: "white",
-    flex: 1,
-    borderRadius: 5,
-    padding: 10,
-    marginRight: 10,
-    marginTop: 17,
-  },
-  emptyDate: {
-    height: 15,
-    flex: 1,
-    paddingTop: 30,
-  },
+
+    container: {
+
+        flex: 1,
+
+    },
+
+    item: {
+
+        flex: 1,
+
+        borderRadius: 5,
+
+        padding: 10,
+
+        marginRight: 10,
+
+        marginTop: 17
+
+    },
+
 });
+
+
+export default App;
